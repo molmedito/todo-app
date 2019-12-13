a678add73dfff305e23a1fe4f5618bc441b31207
Marcelo Olmedo <marcelo.a.olmedo@gmail.com>
2019-12-13 16:29:29 -0300

    Inicio del proyecto

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Components 
+import Home from './components/Home';
+
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +23,9 @@ function App() {
           Learn React
         </a>
       </header>
+
+
+      <Home></Home>
     </div>
   );
 }
